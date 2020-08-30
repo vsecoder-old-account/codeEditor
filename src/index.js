b8e1c631a7aa882c88952f36482b74d454b64198
htmlcssphpjs <63463168+htmlcssphpjs@users.noreply.github.com>
2020-08-30 21:14:03 +0300

    codeEditor

---
+++ src/index.js
@@ -0,0 +1,33 @@
+import { app, BrowserWindow } from 'electron';
+
+if (require('electron-squirrel-startup')) {
+  app.quit();
+}
+
+let mainWindow;
+
+const createWindow = () => {
+  mainWindow = new BrowserWindow({
+    width: 1000,
+    height: 600,
+    icon: __dirname + '/img/logo.png'
+  });
+  mainWindow.loadURL(`file://${__dirname}/index.html`);
+
+  mainWindow.on('closed', () => {
+    mainWindow = null;
+  });
+};
+app.on('ready', createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  if (mainWindow === null) {
+    createWindow();
+  }
+});
